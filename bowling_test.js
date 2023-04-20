b5b8195bba8b37af5c6bfe58f132d7e6d18c15c8
Roman Serebryakov <43926032+SholleZven@users.noreply.github.com>
2023-04-20 20:38:42 +0500

    feat: Add test for Invalid_characters and Invalid_pins

--- bowling_test.js
+++ bowling_test.js
@@ -4,6 +4,17 @@ describe("Bowling", function () {
       bowling_helper("", 0);
       bowling_helper("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", -1);
     });
+    describe("Invalid characters", function() {
+        bowling_helper('ejfbs', -1);
+        bowling_helper('ejfbsXXXXXXXXXXXXXXXXXXXXX', -1);
+        bowling_helper('12023040..12023040.r', -1);
+        bowling_helper('909-9-9-909-9-9-9-9-', -1);
+    });
+    describe("Invalid pins", function() {
+        bowling_helper('38------------------', -1);
+        bowling_helper('91------------------', -1);
+        bowling_helper('5/5/5/555/5/5/5/5/5/X', -1);
+    });
   });
 });
 
